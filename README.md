1329795583a30d5e9e8eb8bde1f1f0253dac697f
The Doan <dangthedoan@gmail.com>
2018-05-17 03:36:15 +0700

    Simple angular ui-router

---
+++ README.md
@@ -0,0 +1,2 @@
+#Angular ui-route simple
+Document [ui-router](https://github.com/angular-ui/ui-router/wiki)
---
+++ js/app.js
@@ -0,0 +1,50 @@
+angular.module("app", ['ui.router']).
+	config(function($stateProvider){
+		$stateProvider.state('contacts', {
+			templateUrl: 'templates/contact.html',
+			resolve: {
+				// Example using function with simple return value.
+				// Since it's not a promise, it resolves immediately.
+				simpleObj:  function(){
+					return {value: 'simple!'};
+				},
+				// Example using function with returned promise.
+				// This is the typical use case of resolve.
+				// You need to inject any services that you are
+				// using, e.g. $http in this example
+				promiseObj:  function($http){
+					// $http returns a promise for the url data
+					return $http({method: 'GET', url: 'assets/contacts.json'});
+				}		
+			},
+			// The controller waits for every one of the above items to be
+			// completely resolved before instantiation. For example, the
+			// controller will not instantiate until promiseObj's promise has 
+			// been resolved. Then those objects are injected into the controller
+			// and available for use.  
+			controller: function($scope, $state, simpleObj, promiseObj) {
+				$scope.title = "My Contacts";
+				$scope.simple = simpleObj.value;
+				$scope.contacts = promiseObj.data;
+				//get custom data
+				$scope.customData1 = $state.current.data.customData1; 
+				$scope.customData2 = $state.current.data.customData2; 
+			},
+			//Custom data to state object
+			data: {
+				customData1: 5,
+				customData2: "blue"
+			}  
+		});
+		$stateProvider.state('home', {
+			template: '<h1>Home Page</h1>'
+		});
+		$stateProvider.state('about', {
+			template: '<h1>About page</h1>'
+		});
+
+	}).
+	controller("MainCtrl", ['$scope', MainCtrl]);
+function MainCtrl($scope) {
+
+}
